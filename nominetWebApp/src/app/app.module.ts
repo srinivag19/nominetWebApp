1d7830fa4b08b0465a746b7ab948befab681553f
Gopinath Srinivasan <srinivag.cse@gmail.com>
2023-10-31 21:08:50 +0000

    input common component and table css added

--- nominetWebApp/src/app/app.module.ts
+++ nominetWebApp/src/app/app.module.ts
@@ -3,9 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
 import { NameServerHeaderComponent } from './name-server-header/name-server-header.component';
 import { NameServerInputCommonsComponent } from './name-server-input-commons/name-server-input-commons.component';
 import { ManageNameServerComponent } from './manage-name-server/manage-name-server.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,15 @@ import { ManageNameServerComponent } from './manage-name-server/manage-name-serv
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      closeButton: true,
+      timeOut: 5000, // 15 seconds
+      positionClass: 'toast-top-center'
+      //progressBar: true,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- nominetWebApp/src/app/name-server-input-commons/name-server-input-commons.component.ts
+++ nominetWebApp/src/app/name-server-input-commons/name-server-input-commons.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AppService, NameServer } from '../app.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-name-server-input-commons',
@@ -6,5 +9,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./name-server-input-commons.component.scss']
 })
 export class NameServerInputCommonsComponent {
+
+  nameServers!: NameServer[];
+  contactForm: FormGroup;
+  formGroup: any;
+  disableAddButton: boolean = false;
+  ipValidator: string = '/^(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/';
+
+  constructor(private nameServerService: AppService, private toastrService: ToastrService) {
+    this.contactForm = new FormGroup({
+      message: new FormControl('', [Validators.required]),
+    });
+  }
+
+  ngOnInit(): void {
+    this.nameServerService.registeredNameServer.subscribe((data: any)=>{
+      this.nameServers=data;
+    });
+  }
+
+  onTextareaChange() {
+    this.disableAddButton = this.contactForm.get('message')?.value !== '';
+  }
+
+
+  addNameServer() {
+    let splitList = this.contactForm.get('message')?.value.split('\n');
+    splitList.forEach((item: string) => {
+      if (item.trim()) {
+        //this.disableAddButton=false;
+        let splitDomainIp = item.split(':');
+        if(splitDomainIp?.length==0 || splitDomainIp?.length==1){
+          this.toastrService.error(': is missing between Nameserver and its IP address. Please use format example "ns1.test.uk: 123.123.123.123"', 'Input Format Error');
+          return;
+        }
+        
+        var rgx = /^[0-9]*\.?[0-9]*$/; 
+        const validateIP = splitDomainIp[1].match(rgx);
+        console.log("val",validateIP);
+        if(this.nameServers.length == 0 || this.nameServers.some(list => list.ip != splitDomainIp[1] || list.name != splitDomainIp[0])) {
+          const obj = {
+            id: Math.random().toString(4),
+            name: splitDomainIp[0],
+            ip: splitDomainIp[1],
+          };
+          this.nameServers.push(obj);
+          this.nameServerService.registeredNameServer.next(this.nameServers);
+          this.toastrService.success('New nameserver(s) added successfully ', 'Success!');
+        } else{
+          this.toastrService.error('Duplicate Nameservers found in your input. Please check. ' +item, 'Error');
+        }
+        
+      }
+    });
+  }
 
 }
