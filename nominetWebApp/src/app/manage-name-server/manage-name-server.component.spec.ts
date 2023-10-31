ea04fcd3f1796d000595fbe51734fa3b752d83e3
Gopinath Srinivasan <srinivag.cse@gmail.com>
2023-10-31 21:34:09 +0000

    add and delete DNS test cases added
    add and delete test cases added

--- nominetWebApp/src/app/manage-name-server/manage-name-server.component.spec.ts
+++ nominetWebApp/src/app/manage-name-server/manage-name-server.component.spec.ts
@@ -1,21 +1,64 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {
+  ComponentFixture,
+  TestBed,
+  async
+} from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
+import { AppService, NameServer } from '../app.service';
 import { ManageNameServerComponent } from './manage-name-server.component';
 
 describe('ManageNameServerComponent', () => {
   let component: ManageNameServerComponent;
   let fixture: ComponentFixture<ManageNameServerComponent>;
+  let appService: AppService;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ManageNameServerComponent],
+      providers: [AppService],
+      imports: [ReactiveFormsModule, ToastrModule.forRoot()],
+    }).compileComponents();
+  }));
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [ManageNameServerComponent]
-    });
     fixture = TestBed.createComponent(ManageNameServerComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    appService = TestBed.get(AppService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should add a name server', () => {
+    const sampleNameServer = [
+      { id: '0.e482173f908b7', name: 'ns1.test.uk', ip: '123.123.123.123' },
+    ];
+    appService.registeredNameServer.next(sampleNameServer);
+    console.log(component.nameServers);
+    appService.registeredNameServer.subscribe((item: NameServer[]) => {
+      fixture.detectChanges();
+      component.nameServers = item;
+      if (component.nameServers.length) {
+        expect(component.nameServers.length).toBe(1);
+        expect(component.nameServers[0].ip).toBe('123.123.123.123');
+        expect(component.nameServers[0].name).toBe('ns1.test.uk');
+      }
+    });
+  });
+
+  it('should delete a name server', () => {
+    const sampleNameServer = [
+      { id: '0.e482173f908b7', name: 'ns1.test.uk', ip: '123.123.123.123' },
+    ];
+    appService.registeredNameServer.next(sampleNameServer);
+    appService.registeredNameServer.subscribe((item: NameServer[]) => {
+      fixture.detectChanges();
+      component.nameServers = item;
+      if (component.nameServers.length) {
+        component.deleteNameServer(component.nameServers[0]?.id);
+      }
+    });
   });
 });
