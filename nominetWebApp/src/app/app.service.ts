8023a739653c7602f50b58f6be4943131353f018
Gopinath Srinivasan <srinivag.cse@gmail.com>
2023-10-31 19:46:36 +0000

    mock apis for domain info and delete service added
    mock apis for domain info and delete service added

---
+++ nominetWebApp/src/app/app.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+
+export class NameServer {
+    "id": string;
+    "name"?: string;
+    "ip"?: string;
+}
+
+@Injectable({
+    providedIn: 'root',
+})
+export class AppService {
+    
+    private nameServers: NameServer[] = [];
+
+
+    deleteNameServer(id: string): void {
+        const serverIndex = this.nameServers.findIndex(
+            (server) => server.id === id
+        );
+        if (serverIndex !== -1) {
+            this.nameServers.splice(serverIndex, 1);
+        }
+    }
+
+    getDomainInfo(): any {
+        return [
+            { domain: 'example.uk', registrar: 'Test Registrar' },
+        ];
+    }
+}
