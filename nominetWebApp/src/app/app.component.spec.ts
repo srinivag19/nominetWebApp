1b1ededeb463d54d637efd3b3e2bc8a9de891ccf
Gopinath Srinivasan <srinivag.cse@gmail.com>
2023-10-31 22:15:37 +0000

    tests support added
    tests support added

--- nominetWebApp/src/app/app.component.spec.ts
+++ nominetWebApp/src/app/app.component.spec.ts
@@ -1,12 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { NameServerHeaderComponent } from './name-server-header/name-server-header.component';
+import { ManageNameServerComponent } from './manage-name-server/manage-name-server.component';
+import { NameServerInputCommonsComponent } from './name-server-input-commons/name-server-input-commons.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
 
-describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
+describe('AppComponent', async () => {
+  await beforeEach(() => TestBed.configureTestingModule({
+    imports: [RouterTestingModule, ReactiveFormsModule, ToastrModule.forRoot()],
+    declarations: [AppComponent, NameServerHeaderComponent,
+      ManageNameServerComponent,
+      NameServerInputCommonsComponent],}).compileComponents();
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -26,4 +33,4 @@ describe('AppComponent', () => {
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain('nominetWebApp app is running!');
   });
-});
+
--- nominetWebApp/src/app/name-server-input-commons/name-server-input-commons.component.spec.ts
+++ nominetWebApp/src/app/name-server-input-commons/name-server-input-commons.component.spec.ts
@@ -2,14 +2,18 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NameServerInputCommonsComponent } from './name-server-input-commons.component';
 
+import { ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
+
 describe('NameServerInputCommonsComponent', () => {
   let component: NameServerInputCommonsComponent;
   let fixture: ComponentFixture<NameServerInputCommonsComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [NameServerInputCommonsComponent]
-    });
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [NameServerInputCommonsComponent],
+      imports: [ReactiveFormsModule, ToastrModule.forRoot()],
+    }).compileComponents();
     fixture = TestBed.createComponent(NameServerInputCommonsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
